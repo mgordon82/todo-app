d828a15d236a0cb3447edcd8416d4a643fd985cc
Matt G <matt@mattrgordon.com>
2024-02-22 23:57:55 -0600

    adding basic details

--- src/Views/DetailView.jsx
+++ src/Views/DetailView.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from 'react';
+// import { useLocation } from 'react-router-dom';
+
 const DetailView = () => {
+  const local = JSON.parse(localStorage.getItem('detailData')) || {};
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    if (local) {
+      setData(local);
+    }
+  }, []);
+
+  const { summary, description, completed, completedDate, createdDate } = data;
   return (
     <div>
       <h2>Details</h2>
+      <p>Summary: {summary}</p>
+      <p>Description: {description}</p>
+      <p>Complete: {completed ? 'Completed' : 'Incomplete'}</p>
+      {completed && <p>Completed On: {completedDate}</p>}
+      <p>Created On: {createdDate}</p>
     </div>
   );
 };
