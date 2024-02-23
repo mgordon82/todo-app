0e03449879dcb25a7d6e3118f080ffeb2668af57
Matt G <matt@mattrgordon.com>
2024-02-22 23:08:52 -0600

    added local storage
    applied check and delete functionality and saved to local storage and checks local storage on load

--- src/Components/ListItems/index.jsx
+++ src/Components/ListItems/index.jsx
@@ -11,8 +11,20 @@ import {
   Typography,
 } from '@mui/material';
 import DeleteIcon from '@mui/icons-material/Delete';
+import InfoIcon from '@mui/icons-material/Info';
 const ListItems = (props) => {
-  const { title, description, action } = props;
+  const { title, description, action, data, setData, completed } = props;
+  const filteredData = data.filter((item) => item.completed === completed);
+  const handleChange = (itemData) => {
+    const newData = data.map((item) =>
+      item.id === itemData.id ? { ...item, completed: !item.completed } : item
+    );
+    setData(newData);
+  };
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
   return (
     <div>
       <Grid container justifyContent='space-between'>
@@ -31,49 +43,64 @@ const ListItems = (props) => {
       </Grid>
 
       <Card variant='outlined' sx={{ marginTop: 1, padding: '5px 10px' }}>
-        {description && (
-          <Typography pl={'10px'} variant='caption'>
-            {description}
-          </Typography>
-        )}
-        <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
-          {[0, 1, 2, 3].map((value, key) => {
-            const labelId = `checkbox-list-label-${value}`;
+        {filteredData.length > 0 ? (
+          <>
+            {description && (
+              <Grid container alignItems='center'>
+                <InfoIcon color='info' fontSize='small' />
+                <Typography pl={'10px'} lineHeight='1em' variant='caption'>
+                  {description}
+                </Typography>
+              </Grid>
+            )}
 
-            return (
-              <ListItem
-                key={value}
-                secondaryAction={
-                  <IconButton edge='end' aria-label='delete'>
-                    <DeleteIcon />
-                  </IconButton>
-                }
-                disablePadding
-              >
-                <ListItemButton
-                  role={undefined}
-                  // onClick={handleToggle(value)}
-                  dense
-                >
-                  <ListItemIcon>
-                    <Checkbox
-                      edge='start'
-                      //   checked={checked.indexOf(value) !== -1}
-                      tabIndex={-1}
-                      disableRipple
-                      inputProps={{ 'aria-labelledby': labelId }}
-                    />
-                  </ListItemIcon>
-                  <ListItemText
-                    id={labelId}
-                    primary={`Line item ${value + 1}`}
-                    secondary={'This is a description'}
-                  />
-                </ListItemButton>
-              </ListItem>
-            );
-          })}
-        </List>
+            <List sx={{ width: '100%', bgcolor: 'background.paper' }}>
+              {filteredData.map((item, key) => {
+                const labelId = `checkbox-list-label-${key}`;
+
+                return (
+                  <ListItem
+                    key={key}
+                    secondaryAction={
+                      <IconButton
+                        edge='end'
+                        aria-label='delete'
+                        onClick={() => handleDelete(item.id)}
+                      >
+                        <DeleteIcon color='error' />
+                      </IconButton>
+                    }
+                    disablePadding
+                  >
+                    <ListItemButton
+                      role={undefined}
+                      // onClick={handleToggle(value)}
+                      dense
+                    >
+                      <ListItemIcon>
+                        <Checkbox
+                          edge='start'
+                          checked={item.completed}
+                          onChange={() => handleChange(item)}
+                          tabIndex={-1}
+                          disableRipple
+                          inputProps={{ 'aria-labelledby': labelId }}
+                        />
+                      </ListItemIcon>
+                      <ListItemText
+                        id={labelId}
+                        primary={item.summary}
+                        secondary={item.description}
+                      />
+                    </ListItemButton>
+                  </ListItem>
+                );
+              })}
+            </List>
+          </>
+        ) : (
+          'No items to show'
+        )}
       </Card>
     </div>
   );
--- src/Views/ListView.jsx
+++ src/Views/ListView.jsx
@@ -1,22 +1,51 @@
 import { Grid } from '@mui/material';
 import ListItems from '../Components/ListItems';
+import todoTempData from '../Data/initialData.json';
+import { useEffect, useState } from 'react';
 
 const ListView = () => {
+  const [todoData, setTodoData] = useState([]);
+  const local = JSON.parse(localStorage.getItem('todoList'));
+
+  useEffect(() => {
+    if (local && local.length > 0) {
+      setTodoData(local);
+    } else {
+      setTodoData([...todoTempData]);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todoList', JSON.stringify(todoData));
+  }, [todoData]);
+
+  console.log('todo data', todoData);
+
   return (
     <Grid sx={{ padding: '20px 0px' }} container spacing={5}>
-      <Grid item xs={12}>
-        <ListItems
-          title='To-Do Items'
-          description='Check each box to mark completed'
-          action
-        />
-      </Grid>
-      <Grid item xs={12}>
-        <ListItems
-          title='Completed Items'
-          description='Check each box to mark incomplete'
-        />
-      </Grid>
+      {todoData && (
+        <>
+          <Grid item xs={12}>
+            <ListItems
+              title='To-Do Items'
+              description='Check each box to mark completed'
+              action
+              completed={false}
+              data={todoData && todoData}
+              setData={setTodoData}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <ListItems
+              title='Completed Items'
+              description='Check each box to mark incomplete'
+              data={todoData && todoData}
+              completed={true}
+              setData={setTodoData}
+            />
+          </Grid>
+        </>
+      )}
     </Grid>
   );
 };
